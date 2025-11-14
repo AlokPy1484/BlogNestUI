a43456fab5d71cd53977ec5cc0e9940e8a5b59f8
AlokPy1484 <alokpandey1483@gmail.com>
2025-11-14 13:03:39 +0530

    resolved merge conflict issue

--- BlogNestUI/src/config.tsx
+++ BlogNestUI/src/config.tsx
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
 export const BASE_URL = "https://blognestapi.onrender.com";
-=======
-// export const BASE_URL = "https://blognestapi.onrender.com";
 
-export const BASE_URL = "http://127.0.0.1:8000";
->>>>>>> Stashed changes
+// export const BASE_URL = "http://127.0.0.1:8000";
